ef59cb71d7f169afe5e0fa64fb382b4044bb1bc5
Thenewchicken55 <Thenewchicken55@users.noreply.github.com>
2026-06-28 03:46:50 +0000

    deploy: ecc0f3d788ecc17acaee65f9fef42bdf17224889

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/AlarmPlaylist/sw.js', { scope: '/AlarmPlaylist/' })})}
